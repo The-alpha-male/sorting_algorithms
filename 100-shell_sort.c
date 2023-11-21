bda4cb0716c942415cc91f36c9bb088961d816ff
will-me-me <willykaria777@gmail.com>
2023-11-21 00:24:12 -0800

    removed the betty issues

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -12,29 +12,30 @@
 
 void shell_sort(int *array, size_t size)
 {
-    size_t i, j, gap = 1;
-    int tmp;
+	size_t i, j, gap = 1;
+	int tmp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    while (gap < size / 3)
-        gap = gap * 3 + 1;
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
 
-    while (gap > 0)
-    {
-        for (i = gap; i < size; i++)
-        {
-            tmp = array[i];
-            j = i;
-            while (j > gap - 1 && array[j - gap] >= tmp)
-            {
-                array[j] = array[j - gap];
-                j -= gap;
-            }
-            array[j] = tmp;
-        }
-        print_array(array, size);
-        gap = (gap - 1) / 3;
-    }
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			tmp = array[i];
+			j = i;
+			while (j > gap - 1 && array[j - gap] >= tmp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+			array[j] = tmp;
+		}
+		print_array(array, size);
+		gap = (gap - 1) / 3;
+	}
 }
+
